78f484ac59dd59257e5fab89bf579bbc024c3d75
jairot <jairotrad@gmail.com>
2013-10-27 22:06:25 -0300

    Basic file Structure

---
+++ dosage.py
@@ -0,0 +1,4 @@
+#!/urs/bin/env python
+#-*- coding: utf-8 -*-
+
+#This Module works as a Dosage Runner that tracks and untracks a tv series
---
+++ dosagedaemon.py
@@ -0,0 +1,5 @@
+#!/urs/bin/env python
+#-*- coding: utf-8 -*-
+
+#This module works as a daemon for the dosage util. 
+#Search in the DB for tracking series
