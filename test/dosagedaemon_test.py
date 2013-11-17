2071cc502c953b5f00d90fa9be30d809497975df
jairot <jairotrad@gmail.com>
2013-11-17 13:35:07 -0300

    two test for the daemon

--- test/dosagedaemon_test.py
+++ test/dosagedaemon_test.py
@@ -6,7 +6,6 @@ from dosagedaemon import DosageDaemon, TorrentClient, TorrentProvider
 from dosage import track, untrack, junky, delete
 
 
-
 class DosageDaemonTest(unittest.TestCase):
 
         @classmethod
@@ -15,7 +14,22 @@ class DosageDaemonTest(unittest.TestCase):
 
         def setUp(self):
             startdb(env='testing')
-            tpmock = Mock(spec=TorrentProvider)
-            tcmock = Mock(spec=TorrentClient)
-            daemon = DosageDaemon(tcmock, tpmock)
+            self.tpmock = Mock(spec=TorrentProvider)
+            self.tcmock = Mock(spec=TorrentClient)
+            self.daemon = DosageDaemon(self.tcmock, self.tpmock)
 
+        def test_empty_db(self):
+            self.daemon.run()
+
+        def test_new_serie(self):
+            track("Mad Men", 1, 0)
+
+            self.daemon.client.already_downloading.return_value = False
+            self.daemon.provider.find.return_value = "MadMenMagnet"
+
+            self.daemon.run()
+
+            self.daemon.client.already_downloading.\
+                        assert_called_once_with("mad men")
+            self.daemon.provider.find.assert_called_once_with("mad men S01E01")
+            self.daemon.client.downdloadassert_called_once_with("MadMenMagnet")
